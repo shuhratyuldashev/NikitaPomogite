ad0a27a8a681af798a1e62043a4b9b218ef1d3c3
Nikita <altair20011@icloud.com>
2024-09-02 17:16:15 +0500

    Изменение Swiper js

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useEffect, useRef } from "react";
 import { GrPrevious } from "react-icons/gr";
@@ -6,11 +6,10 @@ import { GrNext } from "react-icons/gr";
 
 import { register } from "swiper/element/bundle";
 
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Navigation, Pagination, A11y } from "swiper/modules";
-
 const Slider = () => {
   const SwiperEl = useRef(null);
+
+  const [isHydrated, setIsHydrated] = useState(false); // переменная в которо содержится ответ на вопрос произошла ли гидратация JS
 
   useEffect(() => {
     register();
@@ -18,6 +17,7 @@ const Slider = () => {
     const swiperParams = {
       slidesPerView: 1,
       spaceBetween: 50,
+      loop: true, // зацикленность слайдов
       pagination: {
         el: ".swiper-pagination",
         type: "bullets",
@@ -28,69 +28,86 @@ const Slider = () => {
 
     SwiperEl.current.initialize();
     SwiperEl.current.swiper.update();
+
+    setIsHydrated(true); // гидратация произошла
+
+    return () => {
+      setIsHydrated(false); // очистить гидратацию при размонтировании
+    };
   }, []);
 
   return (
-    <swiper-container ref={SwiperEl} init={false} className="Swiper">
-      <div className="swiper-btn__container">
+    <div className="swiper__wrapper">
       <div
-          onClick={() => {
-            SwiperEl.current.swiper.slidePrev();
-          }}
-          className="swiper-btn__prev-btn"
-        ><GrPrevious size={40} color="#333333"/></div>
-
-        <div
-          onClick={() => {
-            SwiperEl.current.swiper.slideNext();
-          }}
-          className="swiper-btn__next-btn"
-        ><GrNext size={40} color="#333333"/></div>
+        onClick={() => {
+          SwiperEl.current.swiper.slidePrev();
+        }}
+        className="swiper-btn__prev-btn"
+      >
+        <GrPrevious size={40} color="#333333" />
       </div>
-      <div class="swiper-pagination" slot="pagination"></div> {/* пагинация */}
-      <swiper-slide>
-        <div className="presentation-site-1">
-          <div className="content">
-            <h1>Лучшие мебели года</h1>
-            <h4>собственное произвотсва</h4>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi hic
-              quaerat <br /> natus quia! Nobis, laboriosam?
-            </p>
-            <button>Перейти</button>
+
+      <div
+        onClick={() => {
+          SwiperEl.current.swiper.slideNext();
+        }}
+        className="swiper-btn__next-btn"
+      >
+        <GrNext size={40} color="#333333" />
+      </div>
+      <swiper-container ref={SwiperEl} init={false} className="Swiper">
+        <div className="swiper-pagination" slot="pagination"></div>
+
+        <swiper-slide>
+          {/* Первый слайд */}
+          <div className="presentation-site-1">
+            <div className="content">
+              <h1>Лучшие мебели года</h1>
+              <h4>собственное произвотсва</h4>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+                hic quaerat <br /> natus quia! Nobis, laboriosam?
+              </p>
+              <button>Перейти</button>
+            </div>
           </div>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <div className="presentation-site-2">
-          <div className="content">
-            <h1>Лучшие мебели года</h1>
-            <h4>собственное произвотсва</h4>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi hic
-              quaerat <br /> natus quia! Nobis, laboriosam?
-            </p>
-            <button>Перейти</button>
-          </div>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <div className="presentation-site-3">
-          <div className="content">
-            <h1>Лучшие мебели года</h1>
-            <h4>собственное произвотсва</h4>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi hic
-              quaerat <br /> natus quia! Nobis, laboriosam?
-            </p>
-            <button>Перейти</button>
-          </div>
-        </div>
-      </swiper-slide>
-    </swiper-container>
+        </swiper-slide>
+        {isHydrated /* осли JS ещё не применился (не произошло гидратации), то мы не показываем остальные слайды (показываем только первый) */ && (
+          <>
+            {/* если хочешь добавить слайды, добавляй в этот контейнер */}
+            <swiper-slide>
+              <div className="presentation-site-2">
+                <div className="content">
+                  <h1>Лучшие мебели года</h1>
+                  <h4>собственное произвотсва</h4>
+                  <br />
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Nisi hic quaerat <br /> natus quia! Nobis, laboriosam?
+                  </p>
+                  <button>Перейти</button>
+                </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+              <div className="presentation-site-3">
+                <div className="content">
+                  <h1>Лучшие мебели года</h1>
+                  <h4>собственное произвотсва</h4>
+                  <br />
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Nisi hic quaerat <br /> natus quia! Nobis, laboriosam?
+                  </p>
+                  <button>Перейти</button>
+                </div>
+              </div>
+            </swiper-slide>
+          </>
+        )}
+      </swiper-container>
+    </div>
   );
 };
 
